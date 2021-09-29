a188e0e2de522d1082e03a10c3b1605a089ad617
Niteshy85013 <niteshyadav85013@gmail.com>
2021-09-29 13:50:35 +0545

    Game over function

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -147,6 +147,7 @@ public class GamePanel extends JPanel implements ActionListener{
         g.setFont( new Font("Ink Free",Font.BOLD, 40));
         FontMetrics metrics1 = getFontMetrics(g.getFont());
         g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics1.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
+
         //Game Over text
         g.setColor(Color.red);
         g.setFont( new Font("Ink Free",Font.BOLD, 75));
